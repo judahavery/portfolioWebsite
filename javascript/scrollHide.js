bb47f67dd9e3c2a0092ce0c182572a09a4301d39
Judah Avery <judah.avery@mymail.champlain.edu>
2020-01-28 14:06:06 -0500

    JS Addition
    Added JS file for navbar functionality

--- javascript/scrollHide.js
+++ javascript/scrollHide.js
@@ -1,9 +1,14 @@
 window.onscroll = function() {scrollFunction()};
 
+const home = document.querySelector('.home');
+const homeSize = home.style.height;
+
 function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
+  if (document.body.scrollTop > homeSize || document.documentElement.scrollTop > homeSize) {
+    document.getElementById("navbar").style.top = "0px";
+  } else if(document.body.scrollTop > 85 || document.documentElement.scrollTop > 85) {
     document.getElementById("navbar").style.top = "-85px";
+  } else {
+    document.getElementById("navbar").style.top = "0px";
   }
 }
