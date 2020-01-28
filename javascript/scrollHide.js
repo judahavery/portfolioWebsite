f96d5b5bca4e5a4ae7a10d57eae21cbb18513da4
Judah Avery <judahbavery@gmail.com>
2020-01-28 15:32:40 -0500

    Slideshow Update
    - Used bootstrap to create a slideshow
    - Navbar z index changed to 1
    - Placeholder content for slideshow

--- javascript/scrollHide.js
+++ javascript/scrollHide.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
+  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
     document.getElementById("navbar").style.top = "0px";
   } else if(document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("navbar").style.top = "-85px";
