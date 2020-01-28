570b9f190c5b8dbe4ba4cf9eca0f3ab534700394
Judah Avery <judahbavery@gmail.com>
2020-01-28 14:32:20 -0500

    Fixed JS Navbar
    - Fixed nav scrolling
    - Adjusted home page size

--- javascript/scrollHide.js
+++ javascript/scrollHide.js
@@ -1,12 +1,9 @@
 window.onscroll = function() {scrollFunction()};
 
-const home = document.querySelector('.home');
-const homeSize = home.style.height;
-
 function scrollFunction() {
-  if (document.body.scrollTop > homeSize || document.documentElement.scrollTop > homeSize) {
+  if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750) {
     document.getElementById("navbar").style.top = "0px";
-  } else if(document.body.scrollTop > 85 || document.documentElement.scrollTop > 85) {
+  } else if(document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("navbar").style.top = "-85px";
   } else {
     document.getElementById("navbar").style.top = "0px";
