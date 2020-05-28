1c3a1c7558425b3eef1cc7e582879be09f6d60dd
Judah Avery <judahbavery@gmail.com>
2020-05-28 10:40:32 -0400

    Started adaptive design

---
+++ javascript/adaptive.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function() {LoadAdaptivePages()});
+
+function LoadAdaptivePages()
+{
+    if (window.innerWidth < 1225) {
+        if (window.location == "index.html")
+        {
+            window.alert("Currently on index.html");
+        }
+        else if (window.location == "cobble.html")
+        {
+
+        }
+        else if (window.location == "neo.html")
+        {
+
+        }
+    }
+}
